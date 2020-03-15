51967e44bdbf4eceffed5b69c5a6bcae3801be9c
kishidashota <shidanemoaki@gmail.com>
2020-03-15 20:59:39 +0900

    devise&views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'reviews/new'
   root "tops#index"
+  resources :posts, only: [ :new, :create, :show]
+  resources :users, only: :show
 end
