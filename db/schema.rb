5eaa9f7f200182ba3c6dacffa3334cb2b09baac2
kishidashota <shidanemoaki@gmail.com>
2020-03-21 22:03:06 +0900

    銘柄で検索　インクリメンタルサーチ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_101137) do
+ActiveRecord::Schema.define(version: 2020_03_21_074514) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_101137) do
     t.text "assortment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["assortment"], name: "index_posts_on_assortment", length: 50
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
