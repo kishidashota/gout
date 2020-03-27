82281675fafb1b96bde03f7b5dd1f84d0fd61aad
kishidashota <shidanemoaki@gmail.com>
2020-03-27 15:24:32 +0900

    js edit post image

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,14 @@
+$(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#food_image').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#image__food").change(function(){
+    readURL(this);
+  });
+ });
