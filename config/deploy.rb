731ceb2cd8bfad9ee5b6dee5299124b3dc5a5b0f
kishidashota <shidanemoaki@gmail.com>
2020-04-12 17:51:49 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,8 @@ lock '3.13.0'
 set :application, 'gout'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kishidashota/gout.git'
+set :repo_url,  'https://github.com/kishidashota/gout.git'
+
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -16,7 +17,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/goutt.pem）'] 
+                  keys: ['~/.ssh/goutt.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
