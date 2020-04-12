afaaff7988cf0ac7dce0c479ac4d5bae8506aa4c
kishidashota <shidanemoaki@gmail.com>
2020-04-13 00:18:54 +0900

    strage-fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,11 +5,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
+
+
     storage :fog
-  end
 
   # storage :fog
 
