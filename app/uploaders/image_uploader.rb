f2ff53e7fbb5e712fc8cfde88c1cf982e7033e49
kishidashota <shidanemoaki@gmail.com>
2020-04-13 00:21:44 +0900

    image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-
-
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
     storage :fog
+  end
 
   # storage :fog
 
