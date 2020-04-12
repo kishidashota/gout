588e7a7fa46ebc0f1e267bc09c1903bf16ccbb74
kishidashota <shidanemoaki@gmail.com>
2020-04-12 21:43:08 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'gout'
+    config.fog_directory  = 'goutt'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gout'
   end
 end
