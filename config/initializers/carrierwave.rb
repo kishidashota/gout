1a4a6ea637874b1a605ffcd59c9e3cd663c1936f
kishidashota <shidanemoaki@gmail.com>
2020-04-12 21:32:26 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,11 +10,11 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',     
-      access_key_id: Rails.application.credentials.access_key_id, 
-      secret_access_key: Rails.application.credentials.secret_access_key,  
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], 
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'goutt'
+    config.fog_directory  = 'gout'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/gout'
   end
 end
